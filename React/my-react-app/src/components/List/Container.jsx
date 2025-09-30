7154754206444455d349c4061daa734bb1396e31
kablt--6362 <wogus6362@gmail.com>
2025-09-30 15:00:23 +0900

    리스트핸들러wnd 고유값 key설정의 중요성

--- React/my-react-app/src/components/List/Container.jsx
+++ React/my-react-app/src/components/List/Container.jsx
@@ -15,7 +15,12 @@ export default function Container() {
   return (
     <div>
       {userArray.map((userElement) => {
-        return <User user={userElement}></User>;
+        // id 는 고유한(2개이상 존재하지않는) 속성
+        // 반복의 대상이 되는 요소에 key 속성과 값을 설정을 해야한다
+        // key에 설정되는 값은 고유한 값이어야 한다.
+        // 반복 랜더링에서 key속성의 역할은
+        // 동일하게 반복 생성된 컴포넌트를 구별(식별)하기위한 속성
+        return <User key={userElement.id} user={userElement}></User>;
       })}
     </div>
   );
