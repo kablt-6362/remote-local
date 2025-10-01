88e7d4b56bce48080d1a1bc6708d6e3da8606846
kablt--6362 <wogus6362@gmail.com>
2025-10-01 14:45:10 +0900

    Stats심화

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import Container from "./components/State/Container";
-import StringState from "./components/State/StringState";
-import NumberState from "./components/State/NumberState";
-import ObjectState from "./components/State/ObjectState";
-import Objv2 from "./components/State/Objv2";
-import ArrayState from "./components/State/ArrayState";
+import Container from "./components/StateeAdvanced/Container";
+
 export default function App() {
   return (
     <div>
-      <StringState></StringState>
-      <NumberState></NumberState>
-      <ObjectState></ObjectState>
-      <Objv2></Objv2>
-      <ArrayState></ArrayState>
+      <Container></Container>
     </div>
   );
 }
