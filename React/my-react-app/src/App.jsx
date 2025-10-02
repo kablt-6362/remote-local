bb92fcf2b3e09605d7f6bc233afd196b2a9b16ce
kablt--6362 <wogus6362@gmail.com>
2025-10-02 17:18:26 +0900

    인스턴스 요청 함수분리.인증문제401/주소문제404

--- React/my-react-app/src/App.jsx
+++ React/my-react-app/src/App.jsx
@@ -5,7 +5,7 @@ import MovieDetail from "./components/TMDB/MovieDetail";
 export default function App() {
   return (
     <div>
-      <MovieDetail></MovieDetail>
+      <MovieList></MovieList>
     </div>
   );
 }
