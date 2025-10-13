9512fcea6525b80e1268466e50c43038e269a071
kablt--6362 <wogus6362@gmail.com>
2025-10-13 14:53:08 +0900

    중첩라우팅

---
+++ my-react-router-app/src/layout/AuthLayout.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+export default function AuthLayout() {
+  //기본 css
+  const baseClass = `border-2 border-amber-300 p-2`;
+  // 활성화된 class
+  const activaClass = `border-2 border-red-900`;
+
+  return (
+    <div>
+      <div className="flex gap-2">
+        {/* jsx보간법 + 템플릿 리터럴 + 화살표 함수 +삼항연산자 혼합 */}
+        <NavLink
+          className={({ isActive }) =>
+            `${baseClass} ${isActive ? activaClass : ""}`
+          }
+          to="/auth"
+        >
+          인증 홈페이지
+        </NavLink>
+        <NavLink className={`${baseClass}`} to="auth/login">
+          인증 로그인
+        </NavLink>
+        <NavLink className={`${baseClass}`} to="auth/signup">
+          인증 회원가입
+        </NavLink>
+        <NavLink to="/">홈페이지</NavLink>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
--- my-react-router-app/src/router/index.js
+++ my-react-router-app/src/router/index.js
@@ -1,7 +1,3 @@
-import Home from "../pages/Home";
-import About from "../pages/About";
-import Profile from "../pages/Profile";
-
 // React Router의 createBrowserRouter 불러오기
 // createBrowserRouter 함수
 // 라우터 설정을 생성하는 주소
@@ -9,19 +5,59 @@ import Profile from "../pages/Profile";
 
 import { createBrowserRouter } from "react-router-dom";
 
+// 레이아웃 컴포넌트 불러오기
+import Rootlayout from "../layout/Rootlayout";
+import AuthLayout from "../layout/AuthLayout";
+
+// 페이지 컴포넌트 불러오기
+import Home from "../pages/RootPages/Home";
+import About from "../pages/RootPages/About";
+import Profile from "../pages/RootPages/Profile";
+//authhome
+import AuthHome from "../pages/Authpages/AuthHome";
+import Login from "../pages/Authpages/Login";
+import SignUp from "../pages/Authpages/SignUp";
+
 // 라우터 설정 생성
 const router = createBrowserRouter([
   {
-    path: "/", //주소(url)
-    Component: Home, //주소에 해당하는 컴포넌트
+    path: "/",
+    Component: Rootlayout,
+    children: [
+      {
+        // 중첩할 자식 경로 객체를 정의하는 배열
+        index: true, // index: true -> 부모경로의 기본 경로
+        // 실제 완성되는 path= path: "/"  (부모경로와 동일)
+        Component: Home,
+      },
+      {
+        path: "about",
+        Component: About,
+      },
+      {
+        path: "profile",
+        Component: Profile,
+      },
+    ],
   },
+  // auth 레이아웃
   {
-    path: "/about",
-    Component: About,
-  },
-  {
-    path: "/profile",
-    Component: Profile,
+    path: "/auth",
+    Component: AuthLayout,
+    children: [
+      {
+        index: true,
+        Component: AuthHome,
+      },
+      {
+        path: "signup",
+        Component: SignUp,
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+    ],
   },
 ]);
 
