6a0caaea28e06ab0bf98f3fec81156acaf9d21d4
kablt-6362 <wogus6362@gmail.com>
2025-10-14 15:33:30 +0900

    패스경로상수적용실패-/에대한 오류 (#6)

--- my-react-router-app/src/constant/paths.js
+++ my-react-router-app/src/constant/paths.js
@@ -1,17 +1,17 @@
 const PATHS = {
   ROOt: {
     INDEX: "/",
-    ABOUT: "about",
-    POSTS: "posts",
-    POST_DETAIL: "posts/postId",
+    ABOUT: "/about",
+    POSTS: "/posts",
+    POST_DETAIL: "/posts/postId",
     // 경로 파라미터를 처리하는 매서드
-    getPostEdtail: (postId) => `posts/${postId}`,
-    PROFILE: "profile",
+    getPostEdtail: (postId) => `/posts/${postId}`,
+    PROFILE: "/profile",
   },
   Auth: {
     INDEX: "/auth",
-    SIGNUP: "signup",
-    LOGIN: "login",
+    SIGNUP: "/signup",
+    LOGIN: "/login",
   },
 };
 export default PATHS;
--- my-react-router-app/src/router/index.js
+++ my-react-router-app/src/router/index.js
@@ -22,63 +22,17 @@ import SignUp from "../pages/Authpages/SignUp";
 import PostDetail from "../pages/RootPages/PostDetail";
 import PostListNew from "../pages/RootPages/PostListNew";
 import PATHS from "../constant/paths";
+import rootRouter from "./routes/rootRoutes";
+import authRouter from "./routes/authroutes";
 
 // 라우터 설정 생성
 const router = createBrowserRouter([
   {
-    path: PATHS.ROOt.INDEX,
-    Component: Rootlayout,
-    children: [
-      {
-        // 중첩할 자식 경로 객체를 정의하는 배열
-        index: true, // index: true -> 부모경로의 기본 경로
-        // 실제 완성되는 path= path: "/"  (부모경로와 동일)
-        Component: Home,
-      },
-      {
-        path: PATHS.ROOt.ABOUT,
-        Component: About,
-      },
-      {
-        path: PATHS.ROOt.POSTS,
-        Component: PostListNew,
-      },
-      {
-        path: PATHS.ROOt.POST_DETAIL,
-        Component: PostDetail,
-      },
-      {
-        //path 속성 x
-        Component: Protectedlayout,
-        //보호할 경로와 컴포넌트 정의
-        children: [
-          {
-            path: PATHS.ROOt.PROFILE,
-            Component: Profile,
-          },
-        ],
-      },
-    ],
+    //스프레드 연산자로 경로 배열을 복사한다
+    ...rootRouter,
+    ...authRouter,
   },
   // auth 레이아웃
-  {
-    path: PATHS.Auth.INDEX,
-    Component: AuthLayout,
-    children: [
-      {
-        index: true,
-        Component: AuthHome,
-      },
-      {
-        path: PATHS.Auth.SIGNUP,
-        Component: SignUp,
-      },
-      {
-        path: PATHS.Auth.LOGIN,
-        Component: Login,
-      },
-    ],
-  },
 ]);
 
 export default router;
