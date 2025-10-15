7ea7080e108da741e5080b2d61c999f764bbff47
kablt--6362 <wogus6362@gmail.com>
2025-10-15 15:18:22 +0900

    불확실한주석제거

--- React/redux-app/src/store/index.js
+++ React/redux-app/src/store/index.js
@@ -6,7 +6,7 @@ import counterReducer from "./counterSlice";
 
 export const store = configureStore({
   reducer: {
-    // 카운터슬라이스 컴포넌트의 reducers를 counter에 저장?
+    
     counter: counterReducer,
   },
 });
